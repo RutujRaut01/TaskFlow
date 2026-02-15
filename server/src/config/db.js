938af3a7e4755bd71f96e83239311ca86b79339b
Your Name <you@example.com>
2026-02-15 23:51:22 +0530

    Fix Render deploy: Support MONGODB_URI env var

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         try { fs.appendFileSync('server_debug.log', 'DB: Attempting connect...\n'); } catch (e) { }
         console.log('Attempting to connect to MongoDB...');
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI, {
             serverSelectionTimeoutMS: 5000
         });
         try { fs.appendFileSync('server_debug.log', `DB: Connected to ${conn.connection.host}\n`); } catch (e) { }
